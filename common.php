14b920a2fe65ee89f7c8b35ccc683b1ed82f9f11
PutinCoin <admin@putincoin.org>
2022-11-25 13:52:46 +0100

    Upload v1.0

---
+++ common.php
@@ -0,0 +1,140 @@
+<?php
+session_start();
+header('Cache-control: private'); // IE 6 FIX
+
+define("WITHDRAWALS_ENABLED", true); //Disable withdrawals during maintenance
+
+include('jsonRPCClient.php');
+include('classes/Client.php');
+include('classes/User.php');
+
+// function by zelles to modify the number to bitcoin format ex. 0.00120000
+function satoshitize($satoshitize) {
+   return sprintf("%.8f", $satoshitize);
+}
+
+// function by zelles to trim trailing zeroes and decimal if need
+function satoshitrim($satoshitrim) {
+   return rtrim(rtrim($satoshitrim, "0"), ".");
+}
+
+$server_url = "server-IP (e.g.: 1.1.1.1) - or server domain, if it has got one";  // website url
+
+$db_host = "localhost";
+$db_user = "mariadbusername";
+$db_pass = "mariadbpassword";
+$db_name = "put";
+
+$rpc_host = "127.0.0.1";
+$rpc_port = "9999";
+$rpc_user = "rpcuser";
+$rpc_pass = "rpcpassword";
+
+$fullname = "PUTinCoin"; //Website Title (Do Not include 'wallet')
+$short = "PUT"; //Coin Short (BTC)
+$blockchain_url = "https://putincoin.info/tx/?"; //Blockchain Url
+$support = "admin@putincoin.org"; //Your support eMail
+$hide_ids = array(1); //Hide account from admin dashboard
+$donation_address = ""; //Donation Address
+
+$fee = "0.0005"; //Set a fee to prevent negative balances. WARNING: This function is incomplete. Setting a fee will show a negative balance for all new user accounts.
+
+if(isSet($_GET['lang']))
+{
+$lang = $_GET['lang'];
+
+// register the session and set the cookie
+$_SESSION['lang'] = $lang;
+
+setcookie('lang', $lang, time() + (3600 * 24 * 30));
+}
+else if(isSet($_SESSION['lang']))
+{
+$lang = $_SESSION['lang'];
+}
+else if(isSet($_COOKIE['lang']))
+{
+$lang = $_COOKIE['lang'];
+}
+else
+{
+$lang = 'en';
+}
+
+switch ($lang) {
+  case 'en':
+  $lang_file = 'lang.en.php';
+  break;
+
+  case 'grc':
+  $lang_file = 'lang.grc.php';
+  break;
+
+  case 'zho':
+  $lang_file = 'lang.zho.php';
+  break;
+
+  case 'ita':
+  $lang_file = 'lang.ita.php';
+  break;
+
+  case 'por':
+  $lang_file = 'lang.por.php';
+  break;
+
+  case 'hin':
+  $lang_file = 'lang.hin.php';
+  break;
+
+  case 'spa':
+  $lang_file = 'lang.spa.php';
+  break;
+
+  case 'tgl':
+  $lang_file = 'lang.tgl.php';
+  break;
+
+  case 'rus':
+  $lang_file = 'lang.rus.php';
+  break;
+
+  case 'nld':
+  $lang_file = 'lang.nld.php';
+  break;
+
+  case 'fra':
+  $lang_file = 'lang.fra.php';
+  break;
+
+  case 'deu':
+  $lang_file = 'lang.deu.php';
+  break;
+
+  case 'tur':
+  $lang_file = 'lang.tur.php';
+  break;
+
+  case 'vie':
+  $lang_file = 'lang.vie.php';
+  break;
+
+  case 'jpn':
+  $lang_file = 'lang.jpn.php';
+  break;
+
+  case 'kor':
+  $lang_file = 'lang.kor.php';
+  break;
+
+  case 'ara':
+  $lang_file = 'lang.ara.php';
+  break;
+
+  default:
+  $lang_file = 'lang.en.php';
+
+}
+
+include_once 'languages/'.$lang_file;
+
+?>
---
+++ webwallet-dump.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.15  Distrib 10.0.38-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: put
+-- ------------------------------------------------------
+-- Server version	10.0.38-MariaDB-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` varchar(300) NOT NULL,
+  `ip` varchar(30) NOT NULL,
+  `username` varchar(65) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `admin` varchar(1) DEFAULT NULL,
+  `locked` varchar(1) DEFAULT NULL,
+  `supportpin` varchar(6) DEFAULT NULL,
+  `secret` varchar(16) DEFAULT NULL,
+  `authused` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2759 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'put'
+--
+
+--
+-- Dumping routines for database 'put'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-25 12:34:24
